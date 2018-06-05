1dd775d499311234d84c9c183892d02f7f9c1fcc
maiareynolds <35492770+maiareynolds@users.noreply.github.com>
2018-06-05 10:59:07 -0400

    Updated from Brython Server: 6/5/2018 10:59:07 AM

--- chapter2.py
+++ chapter2.py
@@ -1,1 +1,13 @@
-
+#Finding the slope, tangent line and normal line of y=x^2 at x=-2:
+x=-2
+y=x**2
+x1=x+.0000000000001
+x2=x-.0000000000001
+y1=x1**2
+y2=x2**2
+m=((y1)-(y2))//(2*.0000000000001)
+t=m*(0-x)+y
+n=(-1/m)*(0-x)+y
+print("Slope =",m)
+print("Tangent equation is y=",m,"(x)+",t)
+print("Normal equation is y=",(-1/m),"(x)+",n)
