a47ae9af9c91a06376d426d0d11db9aaa519f59d
maiareynolds <35492770+maiareynolds@users.noreply.github.com>
2018-06-10 16:54:36 -0400

    Updated from Brython Server: 6/10/2018 4:54:36 PM

--- programming.py
+++ programming.py
@@ -2,10 +2,21 @@
 #Calculus Final
 
 #Limit
+
 #Derivative
+x=int(input("Enter an x value: "))
+x1=x-.0000000000000000000000000000001
+x2=x+.0000000000000000000000000000001
+
+
 #RAM
+
 #Newton's Method
+
 #Euler's
+
 #Length of a Curve
+
 #Taylor Polynomial
+
 #Convergence/Divergence Tests
