ea2d51da43e9acf85e2e85464ea7f6d97cae2e4a
maiareynolds <35492770+maiareynolds@users.noreply.github.com>
2018-06-10 16:45:34 -0400

    Updated from Brython Server: 6/10/2018 4:45:34 PM

--- programming.py
+++ programming.py
@@ -1,1 +1,10 @@
+y1=x1**2
+y2=x2**2
+lim=10000000000000000000000000000
 
+x1=1
+x2=2
+
+fx=(y2-y1)/(x2-x1)
+
+limit derivative RAM Newton Euler Length of a curve Taylor Polynomial Convergence/Divergence Tests
